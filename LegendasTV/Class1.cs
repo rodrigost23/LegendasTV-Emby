f9969dc6261dfde4d3e07c97fec632cd3f6a7982
Rodrigo Tavares <rodrigost23@gmail.com>
2019-11-27 15:14:05 -0400

    Initial plugin configuration

--- LegendasTV/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace LegendasTV
-{
-    public class Class1
-    {
-    }
-}
---
+++ PluginConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Model.Serialization;
+using System;
+
+namespace LegendasTV
+{
+    public class PluginConfiguration : MediaBrowser.Model.Plugins.BasePluginConfiguration
+    {
+    }
+
+    public class Plugin : MediaBrowser.Common.Plugins.BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+
+        }
+
+        public override string Name => "Legendas TV";
+
+        public override string Description => "Get subtitles from Legendas.TV";
+
+        public override Guid Id => new Guid("F54A744C-95DC-499B-A51C-CDD0ECE719B6");
+
+    }
+}
